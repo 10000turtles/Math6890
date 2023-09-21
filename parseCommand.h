653663c1bfafe6b67b8f2d70c1248951055ac7c0
Justin Mucelli <10000turtles@gmail.com>
2023-09-21 18:17:14 -0400

    a

--- parseCommand.h
+++ parseCommand.h
@@ -1,13 +1,54 @@
-#ifndef GET_CPU_H
-#define GET_CPU_H
+#ifndef PARSE_COMMAND_H
+#define PARSE_COMMAND_H
 
-#include <ctime>
-//--------------------------------------------
-// Returnthecurrentwall-clocktimeinseconds
-//--------------------------------------------
-inline double getCPU()
+#include <string>
+using std::string;
+
+//-----------------------------------------------------------------------------------
+// Parseacommandfromastring"line"andreturn ainteger"value"
+// Example:
+// line="-Nx=10"(input)
+// command="-Nx="(input)
+// value=(output)
+// Returnvalue:1ifcommandwasfoundinline
+// 0ifnotfound
+//-----------------------------------------------------------------------------------
+int parseCommand(const string &line, const string &command, int &value)
 {
-    return (1.0 * std::clock()) / CLOCKS_PER_SEC;
+    int len = command.length();
+    if (line.substr(0, len) == command)
+    {
+        sscanf(line.substr(len).c_str(), "%d", &value);
+        printf("parseCommand:SETTING%s%d\n", command.c_str(), value);
+        return 1;
+    }
+    return 0;
+}
+
+// Parseadouble
+int parseCommand(const string &line, conststring &command, double &value)
+{
+    int len = command.length();
+    if (line.substr(0, len) == command)
+    {
+        sscanf(line.substr(len).c_str(), "%le", &value);
+        printf("parseCommand:SETTING%s%e\n", command.c_str(), value);
+        return 1;
+    }
+    return 0;
+}
+
+// Parseastring
+int parseCommand(const string &line, const string &command, string &value)
+{
+    int len = command.length();
+    if (line.substr(0, len) == command)
+    {
+        value = line.substr(len);
+        printf("parseCommand:SETTING%s%s\n", command.c_str(), value.c_str());
+        return 1;
+    }
+    return 0;
 }
 
 #endif
