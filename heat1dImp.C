8699be9eb44230700fffaa02b8c52da347179190
Justin Mucelli <10000turtles@gmail.com>
2023-09-21 18:19:27 -0400

    a

--- heat1dImp.C
+++ heat1dImp.C
@@ -479,7 +479,7 @@ int main(int argc, char *argv[])
     fprintf(matlabFile, "%%Filewrittenbyheat1dImp.C\n");
     fprintf(matlabFile, "xa=%g;xb=%g;kappa=%g;t=%g;maxErr=%10.3e;cpuTimeStep=%10.3e;\n", xa, xb, kappa, tFinal, maxErr, cpuTimeStep);
     fprintf(matlabFile, "Nx=%d;dx=%14.6e;numGhost=%d;n1a=%d;n1b=%d;nd1a=%d;nd1b=%d;\n", Nx, dx, numGhost, n1a, n1b, nd1a, nd1b);
-    fprintf(matlabFile, "solutionName=\’%s\’;\n", solutionName.c_str());
+    fprintf(matlabFile, "solutionName=\'%s\';\n", solutionName.c_str());
 
     writeMatlabVector(matlabFile, x, "x", nd1a, nd1b);
     writeMatlabVector(matlabFile, u[cur], "u", nd1a, nd1b);
