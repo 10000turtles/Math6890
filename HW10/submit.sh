9e2e02d4336f01c62aba0aad42d84458791f5003
Justin Mucelli <10000turtles@gmail.com>
2023-12-01 21:32:58 -0500

    se

---
+++ HW10/submit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash-xe 
+# Usage: 
+# sbatch submit.sh 
+# 
+#SBATCH --job-name=addVector #Jobname 
+#SBATCH --gres=gpu:1 #numberofgpuspernode 
+#SBATCH --ntasks=1 #totalnumberofMPIranks 
+#SBATCH --nodes=1 #Maximumnumberofnodestobeallocated 
+#SBATCH --time=00:01:00 #Walltimelimit(days-hrs:min:sec) 
+#SBATCH --output=addVector%j.log 
+#SBATCH --error=addVector%j.err 
+
+echo "Date =$(date)" 
+echo "Hostname =$(hostname-s)" 
+echo "WorkingDirectory=$(pwd)" 
+echo "" 
+echo "Number ofNodesAllocated = $SLURM_JOB_NUM_NODES" 
+echo "Number ofTasksAllocated = $SLURM_NTASKS" 
+./addVector-n=10000 
+./addVector-n=1000000 
+./addVector-n=100000000
